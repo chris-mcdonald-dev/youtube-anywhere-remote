c91e5604d11b30015e8c57e715e2ff75340f0bda
Chris McDonald <chrislyonsf@gmail.com>
2020-11-13 03:35:25 -0500

    Added new features, fixed lots of bugs
    Features Added:
    - Volume Up
    - Volume Down
    - Loop Current Video
    - User can now easily select which video to control by simply clicking on it (must be the YouTube video furthest to the left)
    
    Functionality Improvements:
    - There is no longer a need to refresh open YouTube tabs after install
    - Improved usability when multiple windows are open
    - Second opened YouTube video in separate window no longer steals focus from first YouTube video opened if first video is still playing
    - Reduced redundant script executions to increase overall speed
    - Minimized amount of uncaught errors
    - Increased communication between the extension and the loaded YouTube page with a MutationObserver in foreground.js

--- background.js
+++ background.js
@@ -1,74 +1,204 @@
-timeoutFlag = true;
-youtubeTabs = []
+// ----------------- MAIN GLOBAL VARIABLES ---------------
+let timeoutFlag = false;
+let updateTimeoutFlag = false;
+let youtubeTabs = [];
+let videoPausedFlag = false;
+let backgroundLoadTimeoutFlag = false;
+
+
+
+// ------------------- FUNCTIONS -----------------------
+
+// Initializes the main script of the program.
+function start() {
+    getAllTabs();
+};
+
+// Makes sure any YouTube tabs already open in any window are accounted for and executes main script when program first runs
+function firstRun() {
+    // Gets every tab open in every window
+    chrome.tabs.query({}, (tabsInAllWindows) => {
+        // filters out Youtube Tabs
+        allYoutubeTabs = tabsInAllWindows.filter(tab => tab.url.includes('youtube.com'));
+        let windowIdArray = [];
+        let windowsDetected = 0; // Used for debug purposes
+        allYoutubeTabs.forEach(tab => {
+            // Pushes each tab's windowId into new array. Also helps to differentiate between which tabs have been worked on and which haven't
+            windowIdArray.push(tab.windowId);
+            for (i = 0; i < windowIdArray.length; i++) {
+                // Here "window" is the BOM window object
+                // Dynamically creates empty array variables named 'browserWindow(i)' for each window open
+                if (!window['browserWindow' + i] && !window[tab.id + 'PushedFlag']) {
+                    window['browserWindow' + i] = [];
+                };
+                if (tab.windowId === windowIdArray[i]) {
+                    if (window['browserWindow' + i]) {
+                        window['browserWindow' + i].push(tab);
+                        // Dynamically creates temporary flag variable for each browser window pushed
+                        window[tab.id + 'PushedFlag'] = true;
+                        // Only executes script on YouTube tabs open furthest to the left in their respective windows
+                        if (tab === window['browserWindow' + i][0]) {
+                            executeScript(tab);
+                            setTimeout(() => videoPausedCheck(tab), 100);
+                            windowsDetected++;
+                        };
+                    };
+                };
+            };
+        });
+        console.log('Number of windows with YouTube videos detected:', windowsDetected);
+        youtubeTabs = allYoutubeTabs; // Temporarily fills youtubeTabs. Helpful to new users on first install. Doesn't require them to refresh any open YouTube tabs.
+    });
+};
+
+function getAllInWindow(currentTab) {
+    chrome.tabs.getAllInWindow(currentTab, allTabs => {
+        // Locates the Youtube Tabs and filters them into new array of objects
+        youtubeTabsExist = allTabs.filter(tab => tab.url.includes('youtube.com'));
+        // Only updates youtubeTabs array if YouTube tabs exist in the window.
+        // Reduces times script is executed, keeping the browser as responsive as possible. Also allows user to refresh windows without youtube tabs and keeps from losing control over windows with videos playing.
+        if (youtubeTabsExist.length != 0) {
+            youtubeTabs = youtubeTabsExist;
+            // Injects foreground.js into Youtube tab of youtubeTabs array furthest to the left.
+            executeScript(youtubeTabs[0]);
+            setTimeout(() => videoPausedCheck(youtubeTabs[0]), 100);
+        }
+        else {
+            console.log('No YouTube tabs were detected in this window.');
+        };
+    });
+};
 
 // Use current tab's info to get info of allTabs with "getAllInWindow"
 function getAllTabs() {
     chrome.tabs.getCurrent(currentTab => {
-        chrome.tabs.getAllInWindow(currentTab, allTabs => {
-            // Locates the Youtube Tabs and filters them into new array of objects
-            youtubeTabsExist = allTabs.filter(tab => tab.url.includes('youtube.com'));
-            // Only updates tab array if YouTube tabs exist in the window.
-            // Reduces times script is executed, keeping the browser as responsive as possible. Also allows user to refresh windows without youtube tabs and keeps from losing control over windows with videos playing.
-            if (youtubeTabsExist.length != 0) {
-                youtubeTabs = youtubeTabsExist
-                // Injects foreground.js into first Youtube tab of youtubeTabs array.
-                chrome.tabs.executeScript(youtubeTabs[0].id, { file: './foreground.js' });
-                console.log('Script executed on:', '"' + youtubeTabs[0].title + '" ', '|  ID:', youtubeTabs[0].id);
-            }
-            else {
-                console.log('No YouTube tabs were detected in this window.');
-            };
-            
-        });
+        getAllInWindow(currentTab);
     });
 };
 
-function commandHandler() {
-    // Sends command to first Youtube tab in the global tab list.
-    chrome.commands.onCommand.addListener(listener = (command) => {
-        // Commands will stack on top of each other if a timeoutFlag is not used
-        // Previous Video command only gets sent when on a YouTube page with a video. (e.g. Not the homepage)
-        if (command === "gPreviousVideo" && youtubeTabs[0].url.includes('youtube.com/watch')) {
-            if (timeoutFlag) {
-                chrome.tabs.goBack(youtubeTabs[0].id);
-                timeoutFlag = false;
-                setTimeout(() => { timeoutFlag = true }, 20);
-            };
-        };
-        if (timeoutFlag) {
-            // Next Video command sends goForward browser-tab command instead of YouTube video "next" key command when not on a YouTube page with a video. (e.g. The homepage)
-            if (youtubeTabs[0].url.includes('youtube.com/watch')) {
-                chrome.tabs.sendMessage(youtubeTabs[0].id, { message: command });
-                console.log('You sent keydown', command, "to", youtubeTabs[0].id);
-                timeoutFlag = false;
-                setTimeout(() => { timeoutFlag = true }, 20);
-            }
-            else if (command === "fNextVideo"){
-                chrome.tabs.goForward(youtubeTabs[0].id);
-                timeoutFlag = false;
-                setTimeout(() => { timeoutFlag = true }, 20);
-            }
+// Executes script on specified tab
+function executeScript(tab) {
+    chrome.tabs.executeScript(tab.id, { file: './foreground.js' });
+    console.log('Script executed on:', '"' + tab.title + '" ', '|  ID:', tab.id);
+};
+
+// Sends message to tab to check if video is paused
+function videoPausedCheck(tab) {
+    chrome.tabs.sendMessage(tab.id, { message: "Is your tab's video playing?" }, (response) => {
+        if (response === "No, you can run a script on the other window.") {
+            videoPausedFlag = true;
         };
     });
-}
+};
 
-function start() {
-    getAllTabs();
-    commandHandler();
-}
 
-// Initiates sequence
-start();
+
+// ------------------- EVENT LISTENERS -----------------------
+
+// Sends command to first Youtube tab in the global tab list.
+chrome.commands.onCommand.addListener(listener = (command) => {
+    // Commands will stack on top of each other if a timeoutFlag is not used
+    // Previous Video command only gets sent when on a YouTube page with a video. (e.g. Not the homepage)
+    if (command === "gPreviousVideo" && youtubeTabs[0].url.includes('youtube.com/watch')) {
+        if (!timeoutFlag) {
+            chrome.tabs.goBack(youtubeTabs[0].id);
+            timeoutFlag = true;
+            setTimeout(() => { timeoutFlag = false }, 20);
+        };
+    };
+    if (!timeoutFlag) {
+        // Next Video command sends goForward browser-tab command instead of YouTube video "next" key command when not on a YouTube page with a video. (e.g. The homepage)
+        if (youtubeTabs[0].url.includes('youtube.com/watch')) {
+            chrome.tabs.sendMessage(youtubeTabs[0].id, { message: command });
+            console.log('You sent keydown', command, "to", youtubeTabs[0].id);
+            timeoutFlag = true;
+            setTimeout(() => { timeoutFlag = false }, 20);
+        }
+        else if (command === "fNextVideo"){
+            chrome.tabs.goForward(youtubeTabs[0].id);
+            timeoutFlag = true;
+            setTimeout(() => { timeoutFlag = false }, 20);
+        };
+    };
+});
 
 // Runs getAllTabs again when any tab is updated
-chrome.tabs.onUpdated.addListener((id, updatedTab) => {
-    // Checks if loading status is complete. This reduces CPU usage by ignoring things like SNS notifications and other minor state changes.
-    if (updatedTab.status === "complete") {
-        start();
-    }
+chrome.tabs.onUpdated.addListener((id, changeInfo, updatedTab) => {
+    if (!updateTimeoutFlag) {
+        // Ignores changes to tabs' audible property
+        if (changeInfo.audible != undefined) return;
+        // Doesn't update youtubeTabs array if user hasn't un-paused already open YouTube video in another window.
+        // This allows users to have multiple windows with YouTube video tabs and have control over which window's YouTube video to send commands to. This keeps control focused on that video even while the user browses the internet in another window with YouTube tabs.
+        try {
+            if ((updatedTab.url.includes('youtube.com/watch') && videoPausedFlag) || youtubeTabs.length === 0) {
+                // Checks if loading status is complete. This reduces CPU usage by ignoring things like SNS notifications and other minor state changes.
+                if (updatedTab.status === 'complete') {
+                    start();
+                    updateTimeoutFlag = true;
+                    setTimeout(() => updateTimeoutFlag = false, 1000);
+                    return;
+                };
+            };
+            if (updatedTab.url.includes('youtube.com/watch') && !videoPausedFlag) {
+                if (updatedTab.status === 'complete') {
+                    executeScript(updatedTab);
+                    backgroundLoadTimeoutFlag = true;
+                    setTimeout(() => backgroundLoadTimeoutFlag = false, 1000);
+                };
+            };
+        }
+        catch (error) {
+            // Ignores very specific error. If having trouble debugging you can try commenting it out. Note the console will be flooded with error whenever empty tab is opened.
+            if (!error.toString().includes("Cannot read property 'windowId' of undefined")) { console.log(error) };
+        };
+    };
 });
 
 // Ensures the tab furthest to the left is first index in array when the user rearranges tabs.
-chrome.tabs.onMoved.addListener(() => {
-    start();
+chrome.tabs.onMoved.addListener((id, movedTab) => {
+    // Needs timeoutFlag due to multiple tab moves being registered when a user rearranges tabs.
+    if (!timeoutFlag) {
+        chrome.tabs.get(id, (movedTab) => {
+            // Does the same thing as .onUpdated but when a tab is moved instead.
+            try {
+                if ((movedTab.windowId === youtubeTabs[0].windowId) || (movedTab.url.includes('youtube.com/watch') && videoPausedFlag)) {
+                    start();
+                    timeoutFlag = true;
+                    setTimeout(() => timeoutFlag = false, 20);
+                };
+            }
+            catch (error) {
+                // See similar error in .onUpdated
+                if (!error.toString().includes("Cannot read property 'windowId' of undefined")) { console.log(error) };
+            };
+        });
+    };
 });
+
+// Runs start() when user un-pauses a video and changes videoPausedFlag accordingly.
+chrome.runtime.onMessage.addListener((videoPaused, sender, sendResponse) => {
+    if (!updateTimeoutFlag) {
+        if (videoPaused) {
+            console.log('Video on', sender.tab.id, 'has been paused.');
+            videoPausedFlag = true;
+        };
+        if (!videoPaused) {
+            console.log('Video on', sender.tab.id, 'has been un-paused and is now playing.');
+            videoPausedFlag = false;
+        };
+        // Prevents control focus from changing to 2nd loaded YouTube video when another video is playing in another window
+        if (backgroundLoadTimeoutFlag && (sender.tab.id != youtubeTabs[0].id) && (youtubeTabs.length != 0)) {
+            return;
+        };
+        // Gets all tabs in window of the video the user last paused instead of video loaded and adds to youtubeTabs array.
+        getAllInWindow(sender.tab.windowId);
+    };
+});
+
+
+
+// ------------------ FUNCTION CALLS -------------------
+
+// Calls initiation functions.
+firstRun();
+start();
--- foreground.js
+++ foreground.js
@@ -1,25 +1,88 @@
 if (!window.scriptInjected) {
-    window.scriptInjected = true;
+    window.scriptInjected = true; //Prevents script from being run multiple times per tab
 
-    console.log('Script has been injected')
+    console.log('Script has been injected');
 
-    console.log(player)
+    domElementCommands = ['dVolumeUp', 'eVolumeDown', 'hLoopCurrentVideo'];
+
+    video = document.querySelector('video');
+
+    // Creates a MutationsObserver to monitor video DOM element
+    const observer = new MutationObserver((mutations) => {
+        // Mutations object used for debugging
+        console.log(mutations);
+        console.log('Video Paused:', video.paused)
+        chrome.runtime.sendMessage(video.paused)
+    });
+    observer.observe(video, {
+        attributes: true
+    })
+
     // Chrome lists Keyboard shortcuts in alphabetical order.
     shortcuts = {
         aPausePlay: new KeyboardEvent('keydown', { 'keyCode': 75 }),
         bRewind: new KeyboardEvent('keydown', { 'keyCode': 37 }),
         cFastforward: new KeyboardEvent('keydown', { 'keyCode': 39 }),
-        dVolumeUp: new KeyboardEvent('keydown', { 'keyCode': 38 }), // Not working yet
-        eVolumeDown: new KeyboardEvent('keydown', { 'keyCode': 40 }), // Not working yet
+        // dVolumeUp works by modifying a DOM element property
+        // eVolumeDown works by modifying a DOM element property
         fNextVideo: new KeyboardEvent('keydown', { 'shiftKey': true, 'keyCode': 78 }),
         // gPreviousVideo works by using the browser tabs API command. Check commandHandler() in background.js
-        hSpeedUp: new KeyboardEvent('keydown', { 'shiftKey': true, 'keyCode': 190 }),
-        iSlowDown: new KeyboardEvent('keydown', { 'shiftKey': true, 'keyCode': 188 }),
+        // hLoopCurrentVideo works by modifying a DOM element property
+        iSpeedUp: new KeyboardEvent('keydown', { 'shiftKey': true, 'keyCode': 190 }),
+        jSlowDown: new KeyboardEvent('keydown', { 'shiftKey': true, 'keyCode': 188 }),
     }
     
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        console.log("Message received: ", request.message)
+        if (domElementCommands.includes(request.message) || Object.keys(shortcuts).includes(request.message)) {
+            console.log("Command received: ", request.message)
+        }
+        // Switch for DOM element commands
+        // !!!Remember to add your new cases to domElementCommands array above!!!
+        switch (request.message) {
+            case "dVolumeUp":
+                if (video.volume < .9) {
+                    video.volume = video.volume + .1;
+                }
+                else {
+                    video.volume = 1
+                };
+                console.log('Volume:', video.volume)
+                break;
+            case "eVolumeDown":
+                if (video.volume > .1) {
+                    video.volume = video.volume - .1;
+                }
+                else {
+                    video.volume = 0
+                };
+                console.log('Volume:', video.volume)
+                break;
+            case "hLoopCurrentVideo": {
+                if (!video.loop) {
+                    video.loop = true;
+                    console.log('Video will now loop');
+                }
+                else {
+                    video.loop = false;
+                    console.log('Video will no longer loop');
+                };
+            };
+        };
         // Uses incoming message as a dynamic key to dispatch keypress
-        document.dispatchEvent(shortcuts[request.message]);
-    })
+        if (Object.keys(shortcuts).includes(request.message)) {
+            document.dispatchEvent(shortcuts[request.message])
+        };
+    });
+    
+    // Prevents background.js from running scripts on other windows when video is still playing in current window.
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.message === "Is your tab's video playing?") {
+            if (!video.paused) {
+                sendResponse("Yes, don't run a script on the other window yet.")
+            }
+            else {
+                sendResponse("No, you can run a script on the other window.")
+            }
+        }
+    });
 }
